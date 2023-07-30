af8eea7043604fe15b2ffaa3009964fd3754cfe4
Morgan Hondros <morganhondros@Morgans-MBP.localdomain>
2023-07-29 22:09:00 -0400

    styles added, divided into overlay and content.

--- src/App.js
+++ src/App.js
@@ -4,13 +4,25 @@ import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import Modal from "react-modal";
 
+Modal.setAppElement("#root");
 function App() {
   // const [modalIsOpen, setModalIsOpen] = useState(false);
   const [modalIsOpen, setModalIsOpen] = useState(false);
   return (
     <div className="App">
       <button onClick={() => setModalIsOpen(true)}>Open Modal</button>
-      <Modal isOpen={modalIsOpen} onRequestClose={() => setModalIsOpen(false)}>
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={() => setModalIsOpen(false)}
+        style={{
+          overlay: {
+            backgroundColor: "grey",
+          },
+          content: {
+            color: "orange",
+          },
+        }}
+      >
         <h2>Modal Title</h2>
         <p>Modal Body</p>
         <button onClick={() => setModalIsOpen(false)}>Close</button>
